ad0f13a1a8aa92e03ff95a38721399da903ba25c
Morgan <bertin.morgan@hotmail.fr>
2022-04-08 09:29:18 -0400

    tripouille

---
+++ tronc_commun/libft/ft_strjoin.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbertin <mbertin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/08 09:17:38 by mbertin           #+#    #+#             */
+/*   Updated: 2022/04/08 09:24:02 by mbertin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strjoin(char const *s1, char const*s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	*str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	while (s1[i] != '\0')
+	{
+		s
+	}
+}
