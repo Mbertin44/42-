bcd4e3c5dfce92de4147e73818b41c1a92f6aade
Morgan <bertin.morgan@hotmail.fr>
2022-04-13 09:07:56 -0400

    ssw

--- tronc_commun/libft/ft_memcpy.c
+++ tronc_commun/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mbertin <mbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 12:43:21 by mbertin           #+#    #+#             */
-/*   Updated: 2022/04/04 15:48:58 by mbertin          ###   ########.fr       */
+/*   Updated: 2022/04/13 09:07:30 by mbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (dst);
 }
+
+/* 	*(unsigned char *)(dst + i) est l'équivalent de dst[i] sauf que je fais 
+	un cast de void vers char */
